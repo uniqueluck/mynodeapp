1a22cead5b381fd2b2c11e52f929aa9644f3bde9
Bhagi <Bhagi979@gmail.com>
2025-04-28 16:39:52 +0000

    commitnewdeploy

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
+index.js
 const express = require('express');
-
 const app = express();
 
-
 // Respond with "Hello World" when a GET request is made to the root "/"
-
 app.get('/', (req, res) => {
-
-  res.send('hey... my Nodejs new App');
-
+  res.send('Hello Nodejs App new');
 });
-
 
 // Start server on port 3000 or custom port via environment variable
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-
   console.log(`Server is running on http://localhost:${PORT}`);
-
 });
 
-
 module.exports = app;
+ 
